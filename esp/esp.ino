a8bea764fba087e13967b7401c3e6195506493c0
Emanuele Sanchi <emanuele.sanchi@studio.unibo.it>
2024-01-18 15:48:08 +0100

    Add Sonar class for distance measurement

--- esp/esp.ino
+++ esp/esp.ino
@@ -1,12 +1,17 @@
+#include <Arduino.h>
+#include "Sonar.h"
 void setup() {
   Serial.begin(115200);
-  Serial.print("setup() is running on core ");
-  Serial.println(xPortGetCoreID());
-  delay(2000);
+  //Serial.print("setup() is running on core ");
+  //Serial.println(xPortGetCoreID());
+  //delay(2000);
 }
 
 void loop() {
-  Serial.print("loop() is running on core ");
-  Serial.println(xPortGetCoreID());
+  //Serial.print("loop() is running on core ");
+  //Serial.println(xPortGetCoreID());
+  Sonar *sonar;
+  //sonar.getDistance();
+  Serial.println(sonar.getDistance());
   delay(1000);
 }
