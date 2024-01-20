8938ee3baebf93aa56fdbe84457381d87eb4eba8
Bacchini Lorenzo <lorenzo.bacchini4@studio.unibo.it>
2024-01-20 19:00:36 +0100

    added main program and state

--- Assignment3_Arduino/src/Gate.cpp
+++ Assignment3_Arduino/src/Gate.cpp
@@ -14,46 +14,29 @@ Gate::Gate(int pin, int open, int close)
 
 void Gate::setOpen()
 {
-    this->pos = 0; // reset the position to 0
-    if(!gateState) {
-        this->servo.attach(pin);
-        for(int i = 0; i < this->open; i++) {
-            this->pos+=DELTA;
-            this->servo.write(pos);
-            delay(6); // small delay to permit the servo to reach the position
-        }
-        gateState = true;
-        this->servo.detach();
-    }
+    this->setPos(open);
 }
 
 void Gate::setClose()
 {
-    this->pos = 90; // reset the position to 90
-    if(gateState) {
-        this->servo.attach(pin);
-        for(int a = 0; a < this->open; a++) {
-            this->pos-=DELTA;
-            this->servo.write(pos);
-            delay(6); // small delay to permit the servo to reach the position
-        }
-        gateState = false;
-        this->servo.detach();
-    }
+    this->setPos(close);
 }
 
 void Gate::setPos(int degrees)
 {
-    int i;
-    (degrees > pos) ? i=+1 : i=-1;
-    this->servo.attach(pin);
-    for(int a = 0; a < abs(degrees-pos); a++) {
-        this->pos+=i;
-        this->servo.write(pos);
-        delay(6); // small delay to permit the servo to reach the position
+    if(degrees != pos){
+        int i;
+        (degrees > pos) ? i=+DELTA : i=-DELTA;
+        this->servo.attach(pin);
+        int j = abs(degrees-pos);
+        for(int a = 0; a < j; a++) {
+            this->pos+=i;
+            this->servo.write(pos);
+            delay(6); // small delay to permit the servo to reach the position
+        }
+        degrees<open ? gateState = false : gateState = true;
+        this->servo.detach();
     }
-    degrees<90 ? gateState = false : gateState = true;
-    this->servo.detach();
 }
 
 int Gate::getPos()
--- Assignment3_Arduino/src/smart_river_monitoring.cpp
+++ Assignment3_Arduino/src/smart_river_monitoring.cpp
@@ -4,8 +4,137 @@
  * @author emanuele.sanchi@studio.unibo.it 		Sanchi Emanuele
  */
 
-#include "Button.h";
-#include "Gate.h";
-#include "LcdMonitor.h";
-#include "Led.h";
-#include "Sonar.h";
+#include "Button.h"
+#include "Gate.h"
+#include "LcdMonitor.h"
+#include "Led.h"
+#include <Arduino.h>
+#include <EnableInterrupt.h>
+
+#define LEDPIN1 10
+#define LEDPIN2 11
+#define LCDROWS 4
+#define LCDCOLS 20
+#define GATEPIN 13
+#define GATEOPEN 180
+#define GATECLOSE 0
+#define BUTTONPIN 2
+
+#define normalLevel 45
+#define alarm_to_highLevel 90
+
+
+Button *button;
+Gate *gate;
+LcdMonitor *lcd;
+Led *ledGreen;
+Led *ledRed;
+
+int manualLevel=90;
+
+volatile enum controllerState {
+    normal,
+    alarm_too_low,
+    pre_alarm_too_high,
+    alarm_too_high,
+    alarm_too_high_critic,
+    manual
+} controllerState;
+
+/**
+ * Setup function, called once when the program starts.
+ */
+void setup()
+{
+	// calibrate sensors.
+    button = new Button(BUTTONPIN);
+    gate = new Gate(GATEPIN, GATEOPEN, GATECLOSE);
+	lcd = new LcdMonitor(LCDROWS, LCDCOLS);
+    ledGreen = new Led(LEDPIN1);
+    ledRed = new Led(LEDPIN2);
+    enableInterrupt(BUTTONPIN, setUnsetManualMode, CHANGE);
+	lcd->setAndPrint("Calibrating sensors", 0, 0);
+    gate->setOpen();
+    delay(1000);
+    gate->setClose();
+	delay(4000);
+
+	Serial.begin(9600);
+    controllerState = normal;
+}
+
+/**
+ * Function called to make the fsm take a step forward over the controllerState.
+*/
+void step(){
+    /*
+    int incomingByte = Serial.read();
+    Serial.println("ciao",DEC);
+    */
+    //read data from River Monitoring Service (state and *level)
+    //*level only if state sent is manual
+    switch(controllerState){
+        case normal:
+            gate->setPos(normalLevel);
+            break;
+        case alarm_too_low:
+            gate->setClose();
+            break;
+        case pre_alarm_too_high:
+            gate->setPos(normalLevel);
+            break;
+        case alarm_too_high:
+            gate->setPos(alarm_to_highLevel);
+            break;
+        case alarm_too_high_critic:
+            gate->setOpen();
+            break;
+        case manual:
+            map(manualLevel, 0, 100, 0, 180);
+            gate->setPos(manualLevel);   
+            break;
+        default:
+            Serial.println("Error: controllerState not valid");
+            break;
+    }
+    lcd->clean();
+    lcd->setAndPrint("Valve opening level:", 0, 0);
+    lcd->setAndPrint(String(map(gate->getPos(), 0, 180, 0, 100))+"%", 0, 1);
+    lcd->setAndPrint("current modality:", 0, 2);
+    lcd->setAndPrint(((controllerState==manual) ? "manual" : "automatic"), 0, 3);
+    //send data to River Monitoring Service (state and level)
+}
+
+/**
+ * Loop function called continuously to make
+ * the fsm take a step forward.
+ */
+void loop()
+{
+    step();
+    delay(10000);
+    controllerState = alarm_too_low;
+    step();
+    delay(10000);
+    controllerState = pre_alarm_too_high;
+    step();
+    delay(10000);
+    controllerState = alarm_too_high;
+    step();
+    delay(10000);
+    controllerState = alarm_too_high_critic;
+    step();
+    delay(10000);
+    controllerState = manual;
+    step();
+    delay(10000);
+    controllerState = normal;
+}
+
+void setUnsetManualMode(){
+    if(controllerState == manual){
+        controllerState = normal;
+    }else{
+        controllerState = manual;
+    }
+}
