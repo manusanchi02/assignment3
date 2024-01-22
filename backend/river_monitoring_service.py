20b7ee34b2d4988e96d25ae4ae4ebe4bcaecdedb
Emanuele Sanchi <emanuele.sanchi@studio.unibo.it>
2024-01-22 18:24:45 +0100

    Changed backend file name

--- backend/river_monitoring_service.py
+++ backend/river_monitoring_service.py
@@ -1,26 +1,27 @@
-import threading
 import time
+import threading
 import paho.mqtt.client as mqtt
+from enum import Enum
 import serial.tools.list_ports
 from http.server import BaseHTTPRequestHandler, HTTPServer
 
 #Enum per stati del sistema
-class State:
+class State(Enum):
     NORMAL = 0
     ALARM_TOO_LOW = 1
     PRE_ALARM_TOO_HIGH = 2
     ALARM_TOO_HIGH = 3
     ALARM_TOO_HIGH_CRITIC = 4
-
+    
 #Costanti per valori frequenza
 F1 = 1000
 F2 = 500
 
 #Costanti per valori soglia
-WL1 = 2
-WL2 = 5
-WL3 = 7
-WL4 = 10
+WL1 = 1
+WL2 = 2
+WL3 = 4
+WL4 = 5
 
 # Impostazioni del broker MQTT
 broker_address = "172.20.10.5"
@@ -32,10 +33,8 @@ topic_receive = "water-level"
 frequency_message = 0
 valve_value = 0
 water_level = 0
-State = State.NORMAL
+state = State.NORMAL
 http_received = ""
-
-water_level_lock = threading.Lock()
 
 # Classe che gestisce le richieste HTTP
 class MyHandler(BaseHTTPRequestHandler):
@@ -86,9 +85,8 @@ def publish_message(client, message):
 # Callback che gestisce la ricezione di un messaggio
 def on_message(client, userdata, msg):
     global water_level
-    with water_level_lock:
-        print(f"Received message on topic {msg.topic}: {msg.payload.decode()}")
-        water_level = float(msg.payload.decode())
+    print(f"Received message on topic {msg.topic}: {msg.payload.decode()}")
+    water_level = float(msg.payload.decode())
 
 # Creazione del client MQTT
 client = mqtt.Client()
@@ -97,57 +95,53 @@ client = mqtt.Client()
 client.connect(broker_address, server_port, 60)
 client.subscribe(topic_receive)
 client.on_message = on_message  # Set the on_message callback
-
-# Start the MQTT loop in a separate thread
-mqtt_thread = threading.Thread(target=client.loop_forever)
-mqtt_thread.start()
+client.loop_start()
 
 # Avvio del server HTTP
 http_thread = threading.Thread(target=run)
 http_thread.start()
 
-
 # Loop principale
 print(water_level)
 try:
     while True:
-        with water_level_lock:
-            print(water_level)
-            if(water_level < WL1):
-                print("Livello acqua troppo basso")
-                frequency_message = F1
-                State = 1
-                valve_value = 0
-            if(water_level > WL1 and water_level < WL2):
-                print("Livello acqua normale")
-                frequency_message = F1
-                State = State.NORMAL
-                valve_value = 25
-            if(water_level > WL2 and water_level <= WL3):
-                print("Livello acqua troppo alto pre-allarme")
-                frequency_message = F2
-                State = State.PRE_ALARM_TOO_HIGH
-                valve_value = 25
-            if(water_level > WL3 and water_level <= WL4):
-                print("Livello acqua troppo alto")
-                frequency_message = F2
-                State = State.ALARM_TOO_HIGH
-                valve_value = 50
-            if(water_level > WL4):
-                print("Livello acqua troppo alto critico")
-                frequency_message = F2
-                State = State.ALARM_TOO_HIGH_CRITIC
-                valve_value = 100
-                
-            # Invia un messaggio con la frequenza desiderata all'esp
-            publish_message(client, frequency_message)
+        print(water_level)
+        if(water_level < WL1):
+            print("Livello acqua troppo basso")
+            frequency_message = F1
+            state = State.ALARM_TOO_LOW.value
+            valve_value = 0
+        if(water_level > WL1 and water_level < WL2):
+            print("Livello acqua normale")
+            frequency_message = F1
+            state = State.NORMAL.value
+            valve_value = 25
+        if(water_level > WL2 and water_level <= WL3):
+            print("Livello acqua troppo alto pre-allarme")
+            frequency_message = F2
+            state = State.PRE_ALARM_TOO_HIGH.value
+            valve_value = 25
+        if(water_level > WL3 and water_level <= WL4):
+            print("Livello acqua troppo alto")
+            frequency_message = F2
+            state = State.ALARM_TOO_HIGH.value
+            valve_value = 50
+        if(water_level > WL4):
+            print("Livello acqua troppo alto critico")
+            frequency_message = F2
+            state = State.ALARM_TOO_HIGH_CRITIC.value
+            valve_value = 100
             
-            # Invia messaggio ad Arduino
-            msg = str(valve_value).encode()
-            #ser.write(msg)
+        # Invia un messaggio con la frequenza desiderata all'esp
+        publish_message(client, frequency_message)
+        
+        # Invia messaggio ad Arduino
+        msg = str(valve_value).encode()
+        ser.write(msg)
+        print("Serial: " + ser.readline().decode())
 
-            # Attendi prima di inviare il prossimo messaggio
-            time.sleep(frequency_message/1000)
+        # Attendi prima di inviare il prossimo messaggio
+        time.sleep(frequency_message/1000)
 
 except KeyboardInterrupt:
     print("Interruzione del loop infinito")
