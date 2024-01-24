19a058c8868413f05b0f9b1e5d712da5d5e502f7
Emanuele Sanchi <emanuele.sanchi@studio.unibo.it>
2024-01-24 12:59:07 +0100

    test

--- backend/test_backend.py
+++ backend/test_backend.py
@@ -1,38 +1,14 @@
-from http.server import BaseHTTPRequestHandler, HTTPServer
-import os
-import threading
-
-#Create custom HTTPRequestHandler class
-class KodeFunHTTPRequestHandler(BaseHTTPRequestHandler):
+#Enum per stati del sistema
+from enum import Enum
+class State(Enum):
+    NORMAL = 0, "Normal"
+    ALARM_TOO_LOW = 1, "Alarm too low"
+    PRE_ALARM_TOO_HIGH = 2, "Pre-alarm too high"
+    ALARM_TOO_HIGH = 3, "Alarm too high"
+    ALARM_TOO_HIGH_CRITIC = 4, "Alarm too high critic"
     
-    #handle GET command
-    def do_GET(self):
-        rootdir = 'c:/xampp/htdocs/' #file location
-        try:
-            #send code 200 response
-            self.send_response(200)
+state = State.NORMAL
 
-            #send header first
-            self.send_header('Content-type','text-html')
-            self.end_headers()
+print("stato: " + str(state.value[1]))    
 
-            #send file content to client
-            self.wfile.write("ciao".encode())
-            return
-            
-        except IOError:
-            self.send_error(404, 'errore')
-    
-def run():
-    print('http server is starting...')
-
-    #ip and port of servr
-    #by default http server port is 80
-    server_address = ('127.0.0.1', 8080)
-    httpd = HTTPServer(server_address, KodeFunHTTPRequestHandler)
-    print('http server is running...')
-    httpd.serve_forever()
-    
-if __name__ == '__main__':
-    http_thread = threading.Thread(target=run)
-    http_thread.start()
+print("stato1: " + str(State.NORMAL.value[1]))
